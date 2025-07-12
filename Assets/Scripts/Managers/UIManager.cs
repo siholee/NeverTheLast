a28a7a3e4591eaf864835fad1ecbff00b1f3ff82
siholee <35533754+siholee@users.noreply.github.com>
2025-07-12 23:46:36 +0900

    250712 UI (2)

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using BaseClasses;
+using UnityEngine;
+using static BaseClasses.BaseEnums;
+
+namespace Managers
+{
+    public class UIManager : MonoBehaviour
+    {
+        // 상단
+        public TMPro.TextMeshProUGUI gameGoldText;
+        public TMPro.TextMeshProUGUI gameLifeText;
+        public TMPro.TextMeshProUGUI gameStageText;
+        public TMPro.TextMeshProUGUI gameSpeedText;
+
+        public void TestButtonClick()
+        {
+            Debug.Log("Test Button Clicked");
+        }
+        
+        // 배속
+        public void OnGameSpeedButtonClick()
+        {
+            string currentText = gameSpeedText.text;
+            string speedString = currentText.Replace("X", "");
+            if (int.TryParse(speedString, out int currentSpeed))
+            {
+                int newSpeed;
+                switch (currentSpeed)
+                {
+                    case 1:
+                        newSpeed = 2;
+                        break;
+                    case 2:
+                        newSpeed = 3;
+                        break;
+                    case 3:
+                        newSpeed = 1;
+                        break;
+                    default:
+                        newSpeed = 1;
+                        break;
+                }
+                
+                gameSpeedText.text = newSpeed + "X";
+            }
+            else
+            {
+                Debug.LogError("게임 속도 텍스트 파싱 실패: " + currentText);
+            }
+        }
+    }
+}
